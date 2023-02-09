4c90cd3729dfaa54d038070fb682b271d1d03aa9
Lucas Masson <lucas.masson001@gmail.com>
2023-02-09 10:43:29 -0300

    Study: Numbers

---
+++ content/numbers.js
@@ -0,0 +1,13 @@
+// Round Number
+let num1 = 1.12332;
+console.log(num1.toFixed(2)); // 1.12
+
+// Verify if a variable is a Integer
+console.log(Number.isInteger(num1)); // false
+
+// Verify if is Not A Number
+console.log(Number.isNaN(num1)); // false
+
+// Parse
+console.log(parseFloat(num1)); // 1.12332
+console.log(parseInt(num1)); // 1
