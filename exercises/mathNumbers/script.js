dcb10bd541e1bf5cc5b451fd302c1143992e4ac3
Lucas Masson <lucas.masson001@gmail.com>
2023-02-09 21:08:05 -0300

    Exercise: almost done

--- exercises/mathNumbers/script.js
+++ exercises/mathNumbers/script.js
@@ -1,5 +1,6 @@
-let num1 = window.prompt("Digite um númer: ");
+let num1 = window.prompt("Digite um número: ");
 
 document.getElementById('1').innerHTML = num1;
+document.getElementById('2').innerHTML = num1;
 document.getElementById('raiz').innerHTML = (num1 ** 0.5).toFixed(2);
-document.getElementById("inteiro").innerHTML = IsInteger(num1);
+document.getElementById("inteiro").innerHTML = Number.isInteger(num1);
